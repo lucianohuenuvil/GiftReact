c1adc8241115369721ec97da4180442018f9ad61
lucianohuenuvil <lucianobvx@gmail.com>
2022-03-16 19:24:27 -0300

    Create array to test hooks

--- gift-app/src/components/GifApp.js
+++ gift-app/src/components/GifApp.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifApp = () => {
 
-  const peliculas = ['Avengers','Harry Potter','Dragon ball z'];
+  //const peliculas = ['Avengers','Harry Potter','Dragon ball z'];
+
+
+  const [peliculas, setPeliculas] = useState(['Avengers','Harry Potter','Dragon ball z'])
+
+  //const [first, setfirst] = useState(second)
+
+  const addFilm = () =>{
+    setPeliculas([...peliculas,'Batman'])
+  }
 
   return (
+
     <div>
+     
+
+        <button onClick={ addFilm }>Agregar Pelicula</button>
+
       {
-        peliculas.map(pelicula => {
-          return <h2 key={pelicula}> {pelicula}</h2>
+        peliculas.map((pelicula, i) => {
+          return <h2 key={i}> {pelicula}</h2>
         })
       }
     </div>
